4ee884d7f4a217f70f6ea2a3376bbd31cd3e9472
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2023-12-03 21:04:45 +0500

    some bugs fixed in front-end

--- main/forms.py
+++ main/forms.py
@@ -11,3 +11,10 @@ class StaffsForm(forms.ModelForm):
     class Meta:
         model = Staffs
         fields = '__all__'
+        widgets = {
+            'name':forms.TextInput(attrs={'placeholder':'Ismi'}),
+            'phone_number':forms.TextInput(attrs={'placeholder':'Telefon raqami'}),
+            'address':forms.TextInput(attrs={'placeholder':'Manzil'}),
+            'salary':forms.NumberInput(attrs={'placeholder':'Oylik'}),
+            'position':forms.NumberInput(attrs={'placeholder':'Lavozimi'}),
+        }
